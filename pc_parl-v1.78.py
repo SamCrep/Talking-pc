14e88068e744d29603bb25bd1852d821b5c6b691
SamCrep <sebastiano.crepaldi2@gmail.com>
2017-05-30 19:13:38 +0200

    Make it look like a typing
    Make the program print the sentence as if it was being typed.

---
+++ pc_parl-v1.78.py
@@ -0,0 +1,9 @@
+import random
+import time
+import math
+##################################################
+def pFrase(frase):
+    curLet = 0
+    for curLet in frase:
+        print(curLet, end = '', flush = True)
+        time.sleep(0.2)
